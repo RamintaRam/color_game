27bd67adfa115e51f119527a568821ce594b27ee
RamintaRam <raminta5@yahoo.com>
2017-08-29 17:48:40 +0300

    splice

--- js/game.js
+++ js/game.js
@@ -11,29 +11,46 @@ function color() {
     var $block;
     var click;
     var $scoreView = $('#score');
+    var ids = ['0-0', '0-1', '0-2', '0-3', '1-0', '1-1', '1-2', '1-3', '2-0', '2-1', '2-2', '2-3', '3-0', '3-1', '3-2', '3-3',];
 
     $view.css('width', size * blockWidth);
     $view.css('height', size * blockWidth);
 
     function fillTable() {
+        var index = ids.splice(1, 1);
+        console.log(index);
         for (i = 0; i < size; i++)
             for (j = 0; j < size; j++) {
-                var rand = colors[Math.floor(Math.random() * colors.length)];
-                var $block = $('<div id="' + i + '-' + j + '" class="block"></div>');
+                // for (i = 0; i < score; i++)
+                //     for (j = 0; j < score; j++) {
+                        ids.splice(1, 1);
+                        ids.splice(3, 1);
+                        ids.splice(5, 1);
+                        ids.splice(7, 1);
+                        ids.splice(9, 1);
+                        ids.splice(11, 1);
+                        ids.splice(13, 1);
+                        ids.splice(15, 1);
 
-                $block.css('width', blockWidth);
-                $block.css('height', blockWidth);
-
-                $view.append($block);
 
 
-                $block.click(function (e) {
+                        var rand = (colors[Math.floor(Math.random() * colors.length)]);
+                        var $block = $('<div id="' + i + '-' + j + '" class="block"></div>');
 
-                    countScore(this.id);
-                });
+                        $block.css('width', blockWidth);
+                        $block.css('height', blockWidth);
 
-                data[i + '-' + j] = rand;
+                        $view.append($block);
 
+
+                        $block.click(function (e) {
+
+                            countScore(this.id);
+                        });
+
+                        data[i + '-' + j] = rand;
+
+                    // }
             }
     }
 
@@ -43,28 +60,24 @@ function color() {
 
         $('#' + id).css('background-color', data[id]);
 
-        if (!click)
-        {
+        if (!click) {
             click = id;
             return;
         }
 
-        if (data[click] === data[id])
-        {
+        if (data[click] === data[id]) {
             score += 1;
             $('#' + click).css('background-color', 'disabled');
             $('#' + id).css('background-color', 'disabled');
             click = null;
         }
-        else
-        {
-            setTimeout(function (){
+        else {
+            setTimeout(function () {
                 $('#' + click).css('background-color', '');
                 $('#' + id).css('background-color', '');
                 click = null;
-            }, 0.9500)
+            }, 900)
         }
-
 
 
         // if($block.click /*&& data[this.id] === data[this.id]*/ )
